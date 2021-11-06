f5185eba7794a7707fa693b92ef0a065491fa460
Sergio Garcia <sergio.gl86@gmail.com>
2021-11-06 13:17:28 +0100

    Se crea el componente Robot con 1er Test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
+import Robot from "./components/Robot/Robot";
 
 function App() {
+  const robot = {
+    caracteristicas: {
+      velocidad: 6,
+      resistencia: 7,
+      FechaCeCreacion: "12-05-2018",
+    },
+    _id: "61858354666bcb02723c195e",
+    nombre: "WALL·E 3",
+    imagenUrl: "https://iresiduo.com/sites/default/files/images/08-Wall-E.jpg",
+  };
+
   return (
     <>
       <h1>Week 6 - Challenge WeekEnd - Robots - REACT</h1>
+      <Robot robot={robot} />
     </>
   );
 }
---
+++ src/components/Robot/Robot.js
@@ -0,0 +1,30 @@
+import "./robot.css";
+
+const Robot = ({ robot }) => {
+  return (
+    <>
+      <li className="robot container-md list-group-item">
+        <div className="row align-items-center">
+          <h2 className="col-12 list-group-item active fs-4">{`Nombre: ${robot.nombre}`}</h2>
+          <div className="row align-items-center">
+            <div className="col-8">
+              <h3 className="fs-6">Caracteristicas:</h3>
+              <h4 className="fs-6">{`Velocidad: ${robot.caracteristicas.velocidad}`}</h4>
+              <h4 className="fs-6">{`Resistencia: ${robot.caracteristicas.resistencia}`}</h4>
+              <h4 className="fs-6">{`Fecha de Creacion: ${robot.caracteristicas.FechaCeCreacion}`}</h4>
+            </div>
+            <div className="col-4">
+              <img
+                className="img-fluid rounded-end"
+                src={robot.imagenUrl}
+                alt={`Imagen de ${robot.nombre}`}
+              />
+            </div>
+          </div>
+        </div>
+      </li>
+    </>
+  );
+};
+
+export default Robot;
