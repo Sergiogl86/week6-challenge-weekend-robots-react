8c44fbd04635bdccac23a96b069f4d2c9435a4b7
Sergio Garcia <sergio.gl86@gmail.com>
2021-11-06 13:48:53 +0100

    Se crea el componente listaRobots

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import "./App.css";
-import Robot from "./components/Robot/Robot";
+import ListaRobots from "./components/ListaRobots/ListaRobots";
 
 function App() {
-  const robot = {
-    caracteristicas: {
-      velocidad: 6,
-      resistencia: 7,
-      FechaCeCreacion: "12-05-2018",
-    },
-    _id: "61858354666bcb02723c195e",
-    nombre: "WALL·E 3",
-    imagenUrl: "https://iresiduo.com/sites/default/files/images/08-Wall-E.jpg",
-  };
-
   return (
     <>
       <h1>Week 6 - Challenge WeekEnd - Robots - REACT</h1>
-      <Robot robot={robot} />
+      <ListaRobots />
     </>
   );
 }
